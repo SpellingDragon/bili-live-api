740169a2b9944074d8b543a3e878a567004b47cb
weiyepeng <weiyepeng@tencent.com>
2023-07-06 23:28:05 +0800

    bugfix:live room

--- websocket/handler_opcode.go
+++ websocket/handler_opcode.go
@@ -7,9 +7,8 @@ import (
 
 	jsoniter "github.com/json-iterator/go"
 
-	"log"
-
 	"github.com/spellingDragon/bili-live-api/dto"
+	"github.com/spellingDragon/bili-live-api/log"
 )
 
 type opCodeHandler func(*dto.WSPayload, *Client)
@@ -26,15 +25,15 @@ func notificationHandler(payload *dto.WSPayload, client *Client) {
 	var handler eventPayloadHandler
 	// HACK 更新后收到的cmd会变为"DANMU_MSG:4:0:2:2:2:0", 在此特殊处理
 	if strings.HasPrefix(eType, "DANMU_MSG") {
-		log.Println("收到CMD消息:", eType, string(payload.Body))
+		log.Debug("收到CMD消息:", eType, string(payload.Body))
 		handler = eventPayloadHandlerMap[dto.EventDanmaku]
 	} else {
 		eventType := dto.EventType(eType)
-		log.Println("收到CMD消息:", eventType, string(payload.Body))
+		log.Debug("收到CMD消息:", eventType, string(payload.Body))
 		var ok bool
 		handler, ok = eventPayloadHandlerMap[eventType]
 		if !ok {
-			log.Println("未知cmd类型: %s", eventType)
+			log.Debugf("未知cmd类型: %s", eventType)
 			handler = unknownEventHandler
 		}
 	}
@@ -48,11 +47,11 @@ func heartbeatResponseHandler(payload *dto.WSPayload, client *Client) {
 	}
 	popularity := binary.BigEndian.Uint32(payload.Body)
 	client.DefaultEventHandlers.Popularity(popularity)
-	log.Println("收到心跳回应,人气值:", popularity)
+	log.Debug("收到心跳回应,人气值:", popularity)
 }
 
 // 进房回应，body为空
 func roomEnterResponseHandler(payload *dto.WSPayload, client *Client) {
 	roomJson, _ := json.Marshal(*client)
-	log.Println("订阅信息：%s", roomJson)
+	log.Infof("订阅信息：%s", roomJson)
 }
