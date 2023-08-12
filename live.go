5b317a26f29124909daddd1fe3f8805f0d46f149
weiyepeng <weiyepeng@tencent.com>
2023-08-12 16:23:39 +0800

    bugfix:room info

--- live.go
+++ live.go
@@ -102,7 +102,7 @@ func (l *Live) enterRoom(roomInfo *resource.RoomInitResp) {
 		return
 	}
 	body, _ := jsoniter.Marshal(dto.WSEnterRoomBody{
-		UID:      1,
+		UID:      602310692,
 		RoomID:   roomInfo.Data.RoomID, // 真实房间ID
 		ProtoVer: 3,                    // 填3
 		Platform: "web",
